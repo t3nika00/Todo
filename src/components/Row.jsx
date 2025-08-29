e4f6cd9b1d28d4bbb74582d5678147e2c08a75a2
Kata <kata.niva03@gmail.com>
2025-08-29 11:14:03 +0300

    First commit

---
+++ src/components/Row.jsx
@@ -0,0 +1,10 @@
+export default function Row({item,deleteTask}){
+    return (
+        <li>
+            {item.description}
+            <button className='delete-button' onClick={() =>
+                deleteTask(item.id)}>Delete</button>
+        </li>
+    )
+    
+}

f13c35591d759d1e84200f98f34ed1ecb1298700
Kata <kata.niva03@gmail.com>
2025-09-01 10:04:51 +0300

    Save

--- server/database.sql
+++ server/database.sql
@@ -12,3 +12,13 @@ insert into task (description) values
 ('Fix the reported bugs'),
 ('Deploy the application to production'),
 ('Conduct a code review with peers');
+
+drop table if exists task;
+
+create table task (
+    id serial primary key,
+    description varchar(255) not null
+);
+
+insert into task (description) values ('My test task');
+insert into task (description) values ('My other test task');
